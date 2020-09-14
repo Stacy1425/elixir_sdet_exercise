62f83ceb4c9d446c1101a6d9fc956bcaf4556d18
Stacy1425 <58537913+Stacy1425@users.noreply.github.com>
2020-09-14 01:41:20 -0400

    Update mix.exs
    docs and test reports dependencies added

--- elixir_sdet_exercise/mix.exs
+++ elixir_sdet_exercise/mix.exs
@@ -7,7 +7,10 @@ defmodule ElixirSdetExercise.MixProject do
       version: "0.1.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      test_coverage: [
+        tool: Coverex.Task
+      ]
     ]
   end
 
@@ -21,7 +24,9 @@ defmodule ElixirSdetExercise.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:hound, "~> 1.0"}
+      {:hound, "~> 1.0"},
+      {:ex_doc,  "~> 0.16"},
+      {:coverex, "~> 1.4.10", only: :test}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
